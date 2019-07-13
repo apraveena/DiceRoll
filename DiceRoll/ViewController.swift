f89f1cfdad10a0658fe00363d3e48d290a10fff1
Praveena <praveena@spuriq.com>
2019-07-13 09:31:38 -0700

    Dice Roll

--- DiceRoll/ViewController.swift
+++ DiceRoll/ViewController.swift
@@ -10,11 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView2: UIImageView!
+    @IBOutlet weak var imageView1: UIImageView!
+    
+    var diceImages = ["Dice Blue 1", "Dice Blue 2", "Dice Blue 3", "Dice Blue 4", "Dice Blue 5", "Dice Blue 6"]
+    var num1 : Int = 0
+    var num2 = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        setImages()
     }
 
-
+    func setImages(){
+        num1 = Int.random(in: 0...5)
+        num2 = Int.random(in: 0...5)
+        imageView1.image = UIImage(named: diceImages[num1])
+        imageView2.image = UIImage(named: diceImages[num2])
+    }
+    
+    @IBAction func rollButtonClick(_ sender: Any) {
+        setImages()
+    }
+    
 }
 
